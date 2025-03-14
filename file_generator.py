90a3ad808fa692873e632ec4e08b1d1e7c51789c
NCasella <nicolascasella02@gmail.com>
2025-03-14 10:02:02 -0300

    added bounds to particle generator

--- file_generator.py
+++ file_generator.py
@@ -1,14 +1,15 @@
 import random 
 
-static_path = 'files/static/'
-dynamic_path = 'files/dynamic/'
+static_path = 'files/static/Static'
+dynamic_path = 'files/dynamic/Dynamic'
 
 def generate_files(n):
     generate_static_file(n)
     generate_dynamic_file(n)
 
 def generate_static_file(n):
-    with open(f'{static_path}file.txt', 'w') as file:
+    with open(f'{static_path}File.txt', 'w') as file:
+        global L
         L = random.randint(50, 150)
         file.write(f'{n}\n{L}\n')
         
@@ -19,7 +20,7 @@ def generate_dynamic_file(n):
     with open(f'{dynamic_path}file.txt', 'w') as file:
         file.write(f'0\n')
         for i in range(n): 
-            x, y = random.randint(0, 100), random.randint(0, 100)
+            x, y = random.randint(0, L-1), random.randint(0, L-1)
             file.write(f'{x} {y}\n')
 
 def main():
